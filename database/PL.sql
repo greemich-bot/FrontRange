570108e7b6e2c47aa1572ec6eedaf8c1446e4d7c
Sammy Alyooi <alyoois@classwork.engr.oregonstate.edu>
2026-03-08 11:39:52 -0700

    add delete_pass, remove __pycache__ and .pyc files from repo and add to .gitignore

--- database/PL.sql
+++ database/PL.sql
@@ -119,5 +119,40 @@ END //
 DELIMITER ;   
 
 -- #############################
--- UPDATE Passes
+-- DELETE Passes
 -- #############################
+DROP PROCEDURE IF EXISTS sp_DeletePass;
+
+DELIMITER //
+CREATE PROCEDURE sp_DeletePass(IN p_id INT)
+BEGIN
+    DECLARE error_message VARCHAR(255); 
+
+    -- error handling
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        -- Roll back the transaction on any error
+        ROLLBACK;
+        -- Propogate the custom error message to the caller
+        RESIGNAL;
+    END;
+
+    START TRANSACTION;
+        -- Deleting corresponding rows from both Skiers table and 
+        --      intersection table to prevent a data anamoly
+        -- This can also be accomplished by using an 'ON DELETE CASCADE' constraint
+        --      inside the SkiersLifts table.
+        DELETE FROM Passes WHERE PassID = p_id;
+        DELETE FROM Skiers WHERE Skiers_SkierID = p_id;
+
+        -- ROW_COUNT() returns the number of rows affected by the preceding statement.
+        IF ROW_COUNT() = 0 THEN
+            set error_message = CONCAT('No matching record found in Skiers for id: ', p_id);
+            -- Trigger custom error, invoke EXIT HANDLER
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = error_message;
+        END IF;
+
+    COMMIT;
+
+END //
+DELIMITER ;
